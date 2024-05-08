69970df6fc829c53d9e9528ad3f399dc86b33f87
jobinben <1021137079@qq.com>
2024-05-08 18:44:12 +0800

    Add: 支持解析名字并转换成带电话的名字

--- src/app.service.ts
+++ src/app.service.ts
@@ -20,4 +20,52 @@ export class AppService {
   private sha1(input: string): string {
     return crypto.createHash('sha1').update(input, 'utf8').digest('hex');
   }
+
+  convertToSuperSubScript(name: string, number: string, position: string = 'sub') {
+    const subscripts = {
+      0: "₀",
+      1: "₁",
+      2: "₂",
+      3: "₃",
+      4: "₄",
+      5: "₅",
+      6: "₆",
+      7: "₇",
+      8: "₈",
+      9: "₉",
+    };
+  
+    const superscripts = {
+      0: "⁰",
+      1: "¹",
+      2: "²",
+      3: "³",
+      4: "⁴",
+      5: "⁵",
+      6: "⁶",
+      7: "⁷",
+      8: "⁸",
+      9: "⁹",
+    };
+  
+    let formattedNumber = '';
+    if (position == "super") {
+      formattedNumber = number.toString().split("").map((digit) => superscripts[digit]).join("");
+    } else {
+      formattedNumber = number.toString().split("").map((digit) => subscripts[digit]).join("");
+    }
+  
+    return `${name}℡${formattedNumber}`;
+  }
+
+  extractNameAndNumber(text: string): string{
+    const pattern = /([\u4e00-\u9fa5]+)(\d+)/g;
+    let match;
+    while ((match = pattern.exec(text)) !== null) {
+      return this.convertToSuperSubScript(match[1], match[2]);
+    }
+    return '输入内容不符合';
+  }
+
+
 }
