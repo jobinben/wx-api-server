8950ffc226c7177a5dc1b773f0b7419674e63cde
jobinben <1021137079@qq.com>
2024-05-28 11:57:02 +0800

    Add: 支持发送图片，支持监听关注事件

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -49,14 +49,43 @@ export class AppController {
     console.log('xmlData: ', xmlData);
 
     let replyTxt = '你好,回复消息中';
-    const receiveMsg = xmlData?.content[0];
-    if (receiveMsg) {
-      if (receiveMsg.length > 30) {
-        replyTxt = '你发送的内容太长啦';
-      } else {
-        replyTxt= this.appService.extractNameAndNumber(receiveMsg);
+    const msgType = xmlData?.msgtype?.[0]; // 小心类型
+    switch (msgType) {
+      case 'text': {
+        const receiveMsg = xmlData?.content?.[0];
+        if (receiveMsg) {
+          // 进群，发送二维码图片
+          if (receiveMsg.length && receiveMsg.includes('gpt')) {
+            const mediaId = 'xVfG8PVKKjvzzGiZ1dO0RMnEW3N8G69YKRpgnlzU39ZPN-s9ssF_3n8S6LxSkvvi'; // 图片id
+            // 构建要响应的 XML 数据
+            const responseXml = `<xml><ToUserName><![CDATA[${xmlData.fromusername[0]}]]></ToUserName><FromUserName><![CDATA[${xmlData.tousername[0]}]]></FromUserName><CreateTime>${Date.now()}</CreateTime><MsgType><![CDATA[image]]></MsgType><Image><MediaId><![CDATA[${mediaId}]]></MediaId></Image></xml>`;
+            res.set('Content-Type', 'application/xml');
+            res.send(responseXml);
+            return;
+          }
+
+          // 其他消息处理
+          if (receiveMsg.length > 30) {
+            replyTxt = '你发送的内容太长啦';
+          } else {
+            replyTxt= this.appService.extractNameAndNumber(receiveMsg);
+          }
+        } 
+        break;
       }
-    } 
+      case 'event': {
+        const eventType = xmlData?.event?.[0];
+        if (eventType === 'subscribe') {
+          replyTxt = '感谢关注～ 如需加入GPT交流群，请回复：gpt';
+        }
+        break;
+      }
+      default: {
+        replyTxt = '抱歉～ 我暂时无法处理该信息，请稍后再试。';
+        break;
+      }
+    }
+    
 
     // 构建要响应的 XML 数据
     const responseXml = `<xml><ToUserName><![CDATA[${xmlData.fromusername[0]}]]></ToUserName><FromUserName><![CDATA[${xmlData.tousername[0]}]]></FromUserName><CreateTime>${Date.now()}</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[${replyTxt}]]></Content></xml>`;
